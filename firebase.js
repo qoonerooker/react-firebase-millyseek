03cb30f7d81dcc23576a68c6fec69e89d1a83a84
qoonerooker <millyseekproject@gmail.com>
2024-04-24 16:13:15 +0900

    設定ファイルからdb設定を読むように修正

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,8 +19,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
 
-// export default analytics;
-export default db;
+export { auth, provider, db };
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,6 @@
 import { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
-
-import { getFirestore } from "firebase/firestore";
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCQRzjT_xyvXudCKelw5vD2Cv-LJD6UrhE",
-  authDomain: "react-firebase-millyseek.firebaseapp.com",
-  projectId: "react-firebase-millyseek",
-  storageBucket: "react-firebase-millyseek.appspot.com",
-  messagingSenderId: "741908456135",
-  appId: "1:741908456135:web:149bf2eeb237c7277854e2",
-  measurementId: "G-BM7MH5ECGM",
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import { db } from "../firebase";
 
 function App() {
   const [users, setUsers] = useState([]);
